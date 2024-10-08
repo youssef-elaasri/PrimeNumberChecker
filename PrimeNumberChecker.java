0a11d439d7a450276c7ea43d15265c48c2961d8a
elaasri <youssefelaasri2002@gmail.com>
2024-10-08 19:38:52 +0200

    adding code

---
+++ PrimeNumberChecker.java
@@ -0,0 +1,26 @@
+public class PrimeNumberChecker {
+
+    // Vérifie si un nombre est premier
+    public boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Retourne la somme des nombres premiers jusqu'à un certain nombre
+    public int sumPrimesUpTo(int limit) {
+        int sum = 0;
+        for (int i = 2; i <= limit; i++) {
+            if (isPrime(i)) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
